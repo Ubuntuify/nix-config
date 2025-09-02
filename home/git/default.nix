40ab28f6d5eb52b4498e0efc757046644722602a
Ryan Salazar <ryanconrad2007@gmail.com>
2025-09-02 23:29:46 +0800

    feat(git): adds lazygit to module

--- home/git/default.nix
+++ home/git/default.nix
@@ -1,7 +1,47 @@
 {
+  # basic git settings
   programs.git = {
     enable = true;
     userEmail = "ryanconrad2007@gmail.com";
     userName = "Ryan Salazar";
+
+    aliases = {
+      "a" = "add -A";
+    };
+  };
+
+  # adds and configures lazygit
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui.nerdFontsVersion = "3"; # enable nerd font glyphs
+
+      tabwidth = 2;
+      border = "single"; # don't use a rounded border
+
+      spinner = {
+        frames = ["⠋" "⠙" "⠹" "⠸" "⠼" "⠴" "⠦" "⠧" "⠇" "⠏"];
+        rate = 80;
+      };
+
+      commit = {
+        signOff = true;
+        autoWrapCommitMessage = true;
+        autoWrapWidth = 72;
+      };
+
+      merging = {
+        manualCommit = true;
+        squashMergeMessage = "Squash merge {{selectedRef}} into {{currentBranch}}";
+      };
+
+      mainBranches = ["main" "master"];
+
+      os = {
+        edit = "nvim {{filename}}";
+        editAtLine = "nvim {{filename}} +{{line}}";
+        editAtLineAndWait = "nvim {{filename}} +{{line}}";
+      };
+    };
   };
 }
