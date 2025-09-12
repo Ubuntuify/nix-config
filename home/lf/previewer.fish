ad564f44c77fa31e1d31927124dc87e51ed8b5b1
Ryan Salazar <ryanconrad2007@gmail.com>
2025-09-13 01:54:26 +0800

    feat(lf): handles symlinks for previewer script

--- home/lf/previewer.fish
+++ home/lf/previewer.fish
@@ -1,30 +1,35 @@
 #!/usr/bin/env fish
 
-set -l MIME (file --mime-type -b "$argv[1]")
+set -fu MIME (file --mime-type -b "$argv[1]")
+
+# If the file is a symlink, find the MIME of the actual file itself, requires GNU coreutils.
+if test $MIME = "inode/symlink"
+  set -fu MIME (file --mime-type -b (readlink -f $argv[1]))
+end
 
 switch $MIME
   # PDF files
-  case \*application/pdf\*
+  case "*application/pdf*"
     pdftotext "$argv[1]" -
 
   # Compression formats
-  case \*application/x-7z-compressed\*
+  case "*application/x-7z-compressed*"
     7zz l "$argv[1]"
-  case \*application/x-tar\*
+  case "*application/x-tar\*"
     tar -tvf "$argv[1]"
-  case \*application/x-compressed-tar\* \*application/x-\*-compressed-tar\*
+  case "*application/x-compressed-tar\*" "*application/x-*-compressed-tar*"
     tar -tvf "$argv[1]"
-  case \*application/vnd.rar\*
+  case "*application/vnd.rar\*"
     7zz l "$argv[1]"
-  case \*application/zip\*
+  case "*application/zip\*"
     unzip -l "$argv[1]"
 
   # Prints any image as a sixel to terminal (won't work if the terminal emulator does not support it.)
-  case image/\*
+  case "image/*"
     chafa -f sixel -s "$argv[2]\x$argv[3]" --animate off --polite on "$argv[1]"
 
   # Gives syntax highlighting to any file that is a text file.
-  case \*text/\*
+  case "*text/*"
     bat --force-colorization --paging=never --style=changes,numbers \
       --terminal-width (math $argv[2] - 3) "$argv[1]" && false
 
