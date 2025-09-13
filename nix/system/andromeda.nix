5e405850791a252664af41853c2132401c09a311
Ryan Salazar <ryanconrad2007@gmail.com>
2025-09-14 03:48:57 +0800

    fix: home-manager support added to Andromeda

--- nix/system/andromeda.nix
+++ nix/system/andromeda.nix
@@ -1,18 +1,27 @@
 {
-  config,
   lib,
   pkgs,
   ...
-}: {
+}: let
+  defaultUser = "ryans";
+in {
   wsl.enable = true;
   wsl.useWindowsDriver = true;
   wsl.docker-desktop.enable = true;
   wsl.startMenuLaunchers = true;
 
-  wsl.defaultUser = "ryans";
+  wsl.defaultUser = defaultUser;
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+
+  home-manager.users.${defaultUser} = lib.mkMerge [
+    {
+      hm-options.user = defaultUser;
+      hm-options.isGraphical = false;
+    }
+    ../../home/default.nix
+  ];
 
   networking.hostName = "Andromeda";
 
