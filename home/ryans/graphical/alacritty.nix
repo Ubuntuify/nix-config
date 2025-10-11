af7e5b609ea70be67f655d24d239762d59a84989
Ryan Salazar <ryanconrad2007@gmail.com>
2025-10-11 19:13:25 +0000

    alacritty: change font to FantasqueSans

--- home/ryans/graphical/alacritty.nix
+++ home/ryans/graphical/alacritty.nix
@@ -48,22 +48,22 @@ in
             };
 
             normal = {
-              family = "JetBrainsMono Nerd Font Mono";
+              family = "FantasqueSansM Nerd Font Mono";
               style = "Regular";
             };
 
             bold = {
-              family = "JetBrainsMono Nerd Font Mono";
+              family = "FantasqueSansM Nerd Font Mono";
               style = "Bold";
             };
 
             italic = {
-              family = "JetBrainsMono Nerd Font Mono";
+              family = "FantasqueSansM Nerd Font Mono";
               style = "Italic";
             };
 
             bold_italic = {
-              family = "JetBrainsMono Nerd Font Mono";
+              family = "FantasqueSansM Nerd Font Mono";
               style = "Bold Italic";
             };
           };
@@ -75,5 +75,5 @@ in
       ];
     };
 
-    home.packages = [pkgs.nerd-fonts.jetbrains-mono];
+    home.packages = [pkgs.nerd-fonts.fantasque-sans-mono];
   }
