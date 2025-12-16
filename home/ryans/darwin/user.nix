991b8d42e3c2093fcc1b3e4fa12fa8c03e0d91e0
Ryan Salazar <ryanconrad2007@gmail.com>
2025-12-16 17:26:59 +0800

    feat(user/darwin): adds overrideable username, as nix is not managing the whole system

--- home/ryans/darwin/user.nix
+++ home/ryans/darwin/user.nix
@@ -1,4 +1,4 @@
-{outputs, ...}: let
+{outputs, config, lib, ...}: let
   user = "ryan";
 in {
   # Windows (which I derive my NixOS username from) has different naming
@@ -6,21 +6,26 @@ in {
   #
   # Such as the fact that the last name is not included as part of the
   # "username" part of it.
-
-  users.users.${user} = {
-    # nix-darwin's "The Plan" should allow me to set more configuration, such as Docks,
-    # Preferences, and other settings in here later on. You can probably do with
-    # home-manager, but I prefer my setups more specialized on MacOS, and make sure
-    # MacOS specific changes in a different file.
-    #
-    # - tracking issue: nix-darwin/nix-darwin#1452 -
-
-    name = "${user}";
-    description = "Ryan Salazar";
-    home = "/Users/${user}";
+  options.custom.overrideUsername = lib.mkOption {
+    type = lib.types.string;
   };
 
-  home-manager.users.${user} = outputs.lib.system.mkHomeEntry {
-    user = "ryans"; # use *this* specific configuration
+  config = {
+    users.users.${user} = {
+      # nix-darwin's "The Plan" should allow me to set more configuration, such as Docks,
+      # Preferences, and other settings in here later on. You can probably do with
+      # home-manager, but I prefer my setups more specialized on MacOS, and make sure
+      # MacOS specific changes in a different file.
+      #
+      # - tracking issue: nix-darwin/nix-darwin#1452 -
+
+      name = "${user}";
+      description = "Ryan Salazar";
+      home = "/Users/${user}";
+    };
+
+    home-manager.users.${user} = outputs.lib.system.mkHomeEntry {
+      user = "ryans"; # use *this* specific configuration
+    };
   };
 }
