37ebaf169095bf5a5a88717539e4d89258599ca8
Ryan Salazar <ryanconrad2007@gmail.com>
2025-12-16 17:27:59 +0800

    docs(home): adds some comments

--- home/common/home-manager/options.nix
+++ home/common/home-manager/options.nix
@@ -1,5 +1,7 @@
 {lib, ...} @ args: {
   options.custom = {
+    # Machine-specific options, such as mitigating for low RAM or whether graphical applications should be enabled
+    # in the config
     machine = {
       graphics = let
         isNixDarwin = builtins.hasAttr "darwinConfig" args;
@@ -7,10 +9,14 @@
         lib.mkOption {
           type = lib.types.bool;
           description = "Whether to enable graphical applications, such as Firefox or Alacritty.";
-          default = args.nixosConfig.hardware.graphics.enable or isNixDarwin;
+          default = args.nixosConfig.hardware.graphics.enable or isNixDarwin; # this is making an assumption that
+          # all nix-darwin setups are for development only and not for server use.
         };
+
       isLowRam = lib.mkEnableOption "some low RAM mitigation features, such as auto-tab-discard, etc.";
     };
+
+    # Linux-specific options, these have no effect when not on a Linux machine.
     linux = {
       window-manager = lib.mkOption {
         type = lib.types.enum ["sway" "hyprland" null];
